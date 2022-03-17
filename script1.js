05730df456d5e6232a4b09df89d927cef31cb3d9
Sukritan Gupta <20051871@kiit.ac.in>
2022-03-17 22:57:49 +0530

    after completion of home page

---
+++ script1.js
@@ -0,0 +1,18 @@
+function aman(){
+    let a=document.getElementById("mahajan1");
+    let b=document.getElementById("mahajan2");
+    let c=document.getElementById("mahajan3");
+    if(a.style.display==="none"&&   b.style.display==="none"&&c.style.display==="none" ){
+        a.style.display="block";
+        b.style.display="block";
+        c.style.display="block";
+    }
+    
+      
+    
+    else{
+        a.style.display="none";
+        b.style.display="none"
+        c.style.display="none"
+    }
+}

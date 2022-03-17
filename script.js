a6b84aa2c0f9e2fccc0d33a0b56467b39489aa10
Sukritan Gupta <20051871@kiit.ac.in>
2022-03-17 18:38:53 +0530

    half completion of main function

---
+++ script.js
@@ -0,0 +1,24 @@
+let a=document.getElementById('suk1');
+if(a.innerText.length>30){
+    a.innerText=a.innerText.slice(0,50)+'......';
+}
+let b=document.getElementById('suk2');
+if(b.innerText.length>30){
+    b.innerText=b.innerText.slice(0,50)+'......';
+}
+let c=document.getElementById('suk3');
+if(c.innerText.length>30){
+    c.innerText=c.innerText.slice(0,50)+'......';
+}
+let d=document.getElementById('suk4');
+if(d.innerText.length>30){
+    d.innerText=d.innerText.slice(0,50)+'......';
+}
+let e=document.getElementById('suk5');
+if(e.innerText.length>30){
+    e.innerText=e.innerText.slice(0,50)+'......';
+}
+let f=document.getElementById('suk6');
+if(f.innerText.length>30){
+    f.innerText=f.innerText.slice(0,50)+'......';
+}
